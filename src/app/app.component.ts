8f6ef2ce02ec044a4c8a69f2cfbc9f9369a90672
Rizky Rahman Salam <clashikik@gmail.com>
2025-05-22 13:11:32 +0700

    feat: add hero component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
-    HeaderComponent
+    HeaderComponent,
+    HeroComponent
   ],
   template: `
     <div class="app-container">
       <app-header></app-header>
       <main>
+        <app-hero></app-hero>
       </main>
     </div>
   `,
