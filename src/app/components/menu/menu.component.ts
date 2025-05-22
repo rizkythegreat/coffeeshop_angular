8908217c339b3ae06548bb0303ee21e32d48b95c
Rizky Rahman Salam <clashikik@gmail.com>
2025-05-22 14:25:10 +0700

    feat: add menu component

---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+    selector: "app-menu",
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: "",
+    styleUrls: [""],
+})
+export class MenuComponent {}
