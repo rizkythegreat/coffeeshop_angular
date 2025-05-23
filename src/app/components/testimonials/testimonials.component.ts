2ade9bed827ff4c58e9eaa6c6a93197927a8fb77
Rizky Rahman Salam <clashikik@gmail.com>
2025-05-23 13:48:55 +0700

    feat: add testimonial component

---
+++ src/app/components/testimonials/testimonials.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  selector: "app-testimonials",
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: "./testimonials.component.html",
+  styleUrls: ["./testimonials.component.scss"]
+})
+export class TestimonialsComponent {}
