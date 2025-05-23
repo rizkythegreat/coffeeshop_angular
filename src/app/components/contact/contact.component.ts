3e6d71f0b4b70d8be98afb4306e65ec2e132fe7e
Rizky Rahman Salam <clashikik@gmail.com>
2025-05-23 14:01:34 +0700

    feat: add contact functionality

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -9,4 +9,27 @@ import { FormsModule } from "@angular/forms";
     templateUrl: "./contact.component.html",
     styleUrls: ["./contact.component.css"],
 })
-export class ContactComponent {}
+export class ContactComponent {
+  formData = {
+    name: '',
+    email: '',
+    message: ''
+  };
+  
+  formSubmitted = false;
+  
+  onSubmit(): void {
+    console.log('Form submitted', this.formData);
+    this.formSubmitted = true;
+    
+    // Reset form after 3 seconds
+    setTimeout(() => {
+      this.formData = {
+        name: '',
+        email: '',
+        message: ''
+      };
+      this.formSubmitted = false;
+    }, 3000);
+  }
+}
